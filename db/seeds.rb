f646149e11e7c1e76062754d0ae0a44604453775
Phuvit Jaruratkit <poohphuvit@hotmail.com>
2023-05-12 14:47:25 +0900

    seding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,18 @@
 List.destroy_all
 Movie.destroy_all
 puts "Database cleaned"
-url = "http://tmdb.lewagon.com/movie/top_rated"
-10.times do |i|
+url = "http://tmdb.lewagon.com/movie/top_rated?api_key=344f0f780755bea2b31e065cd88c6cc0"
+
+50.times do |i|
   puts "Importing movies from page #{i + 1}"
-  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)["results"]
+  movies = JSON.parse(URI.open("#{url}&page=#{i+1}").read)["results"]
   movies.each do |movie|
     puts "Creating #{movie["title"]}"
-    base_poster_url = "https://image.tmdb.org/t/p/original"
+    base_poster_url = "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}.jpg"
     Movie.create(
-      title: movie["title"],
+      title: movie["original_title"],
       overview: movie["overview"],
-      poster_url: "#{base_poster_url}#{movie["backdrop_path"]}",
+      poster_url: base_poster_url,
       rating: movie["vote_average"]
     )
   end
